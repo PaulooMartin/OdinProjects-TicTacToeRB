67fdb967325be430942769d39173a969c4957748
Paul Martin <paulmartinblopez@gmail.com>
2023-05-02 14:33:01 +0800

    Add temp methods*
    * game_board takes in its players
    * add temp method that starts game and
    * calls for player turn

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,30 +1,52 @@
 class Game
-  def initialize
+  def initialize(player1, player2)
     @board = Array.new(3) { Array.new(3, '-') }
+    @player1 = player1
+    @player2 = player2
   end
 
-  def show
+  def show_board
     @board.each { |row| puts row.join(' ')}
+    puts "\n"
+  end
+
+  def game_commence
+    show_board
+    player_turn(@player1)
+    player_turn(@player2)
+  end
+
+  private
+
+  def player_turn(player)
+    coords = player.turn
+    @board[coords[0].to_i][coords[1].to_i] = player.symbol
+    show_board
   end
 end
 
 class Player
   attr_reader :symbol
 
-  def initialize(symbol)
+  def initialize(name, symbol)
+    @name = name
     @symbol = symbol
   end
 
   def turn
-    self.get_coordinates
+    get_coordinates
   end
 
   private
+
   def get_coordinates
+    puts "#{@name}'s turn"
     coordinate1, coordinate2 = gets.chomp.split(',')
   end
 end
 
-player_1 = Player.new('X')
-player_2 = Player.new('O')
-game_board = Game.new
+player1 = Player.new('Player 1', 'X')
+player2 = Player.new('Player 2', 'O')
+game_board = Game.new(player1, player2)
+
+game_board.game_commence
