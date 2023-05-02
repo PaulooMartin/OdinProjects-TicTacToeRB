d0267b4419d0784fb7eb79d26b7bc536eac080df
Paul Martin <paulmartinblopez@gmail.com>
2023-05-02 12:44:49 +0800

    Add player class and instances

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,2 +1,11 @@
+class Player
+  def initialize(symbol)
+    @symbol = symbol
+  end
+end
+
 game_board = Array.new(3) { Array.new(3, '-') }
 game_board.each { |row| puts row.join(' ') }
+
+player_1 = Player.new('X')
+player_2 = Player.new('O')
